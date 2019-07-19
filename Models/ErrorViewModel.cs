55f1cd92abf3241a4882a18a82d40f28ee7be180
Federico Kurganoff <kfederico_matias@hotmail.com>
2019-07-19 00:11:46 -0300

    Probando commits y esas cosas

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuitaAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
